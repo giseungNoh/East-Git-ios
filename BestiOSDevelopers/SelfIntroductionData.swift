5705d3fbf86a2a598383dfa4dcedab963d5d2a25
노기승 <juks8666@gmail.com>
2025-05-07 17:46:37 +0900

    소개 변경

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -213,11 +213,11 @@ let people: [SelfIntroduction] = [
         name: "노기승",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
         hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
+        mbti: "INFP",
         strength: "ㄴㅁㄴㅇ ㅇㅇㅇ",
         sns: nil,
         etc: "열심히 수료하고 취업하겠습니다!",
-        job: "구글 - iOS 개발자"
+        job: "네이버 - iOS 개발자"
     ),
     SelfIntroduction(
         name: "김현식",
