1b6cee9bcaff34c7b6878f0e1c077d3faa56f6e5
노기승 <juks8666@gmail.com>
2025-05-08 18:29:03 +0900

    ㅇㄴㅇ

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -206,7 +206,7 @@ let people: [SelfIntroduction] = [
         mbti: "INTJ",
         strength: "끈기와 실행력",
         sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
+        etc: "열심히 수료하ㄴㅇㅇ겠습니다!",
         job: "마이크로소프트 - iOS 개발자"
     ),
     SelfIntroduction(
